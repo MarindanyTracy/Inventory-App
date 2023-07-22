eb79adc82a39f9fbf9bf379dcb00a7646b18fc46
MarindanyTracy <marindanytracy0@gmail.com>
2023-07-22 14:06:37 +0300

    Register new user

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -1,13 +1,43 @@
 const User = require("../models/userModel");
+const asyncHandler = require('express-async-handler');
 
-const registerUser = (req, res) => {
-  if(!req.body.email) {
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+
+  // Validation
+  if (!name || !email || !password) {
     res.status(400);
-    throw new Error("Please add an email")
+    throw new Error("Please fill in all required fields");
   }
-  res.send("Register User")
-}
+  if (password.length < 6) {
+    res.status(400);
+    throw new Error("Password must be up to 6 characters");
+  }
+
+  // Check if email already exists
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    res.status(400);
+    throw new Error("User with that email already exists. Please log in");
+  }
+
+  // Create new user
+  const user = await User.create({
+    name,
+    email,
+    password,
+  });
+
+  if (user) {
+    const { _id, name, email, photo, phone, bio } = user;
+    res.status(201).json({ _id, name, email, photo, phone, bio });
+  } else {
+    res.status(400);
+    throw new Error("Invalid user data");
+  }
+});
 
 module.exports = {
-  registerUser
-}
+  registerUser,
+};
