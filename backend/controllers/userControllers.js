6059bc5dabffeeed009cbd841c7f8b56e5c5343a
MarindanyTracy <marindanytracy0@gmail.com>
2023-07-22 13:38:24 +0300

    Create custom error handler

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -1,5 +1,13 @@
 const User = require("../models/userModel");
 
-export const registerUser = async(req, res) => {
+const registerUser = (req, res) => {
+  if(!req.body.email) {
+    res.status(400);
+    throw new Error("Please add an email")
+  }
   res.send("Register User")
+}
+
+module.exports = {
+  registerUser
 }
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -2,20 +2,20 @@ const mongoose = require("mongoose");
 
 const userSchema = mongoose.Schema({
   name : {
-    trype: String,
+    type: String,
     required: [true, 'Please add a name']
   },
   email: {
-    trype: String,
+    type: String,
     required: [true, 'Please add an email'],
     unique: true,
     trim: true,
     match: [
-      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      '/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/'
       , "Please add a valid email"]
   },
   password : {
-    trype: String,
+    type: String,
     required: [true, 'Please add a password'],
     minLength: [6, "Password must be upto 6 characters"],
     maxLength: [23, "Password is too long"],
