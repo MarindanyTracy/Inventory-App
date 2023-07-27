fcada94090d1f030a7d39e22138d932d8bdede2f
MarindanyTracy <marindanytracy0@gmail.com>
2023-07-27 12:43:33 +0300

    Finishing up on forgot password controller function

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -4,6 +4,7 @@ const jwt = require("jsonwebtoken");
 const crypto = require("crypto");
 const bcrypt = require("bcryptjs");
 const Token = require("../models/tokenModel");
+const sendEmail = require("../utils/sendEmail");
 
 const generateToken = (id) => {
   return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "1d" });
@@ -204,6 +205,12 @@ const forgotPassword = asyncHandler(async (req, res) => {
     res.status(404);
     throw new Error("User does not exist!");
   }
+  //Delete token if it exists in db
+  let token = await Token.findOne({userId: user._id})
+  if(token) {
+    await token.deleteOne()
+  }
+  
   //Create reset token
   let resetToken = crypto.randomBytes(32).toString("hex") + user._id;
 
@@ -233,9 +240,21 @@ const forgotPassword = asyncHandler(async (req, res) => {
   <a href=${resetUrl} clicktracking=off>${resetUrl}</a>
   <p>Regards...</p>
   <p>Invent Team</p>
-  `
+  `;
 
-  res.send("Forgot pass");
+  const subject = "Password Reset Request";
+
+  const send_to = user.email;
+  const sent_from = process.env.EMAIL_USER
+
+  try {
+    await sendEmail(subject, message, send_to, sent_from);
+    res.status(200).json({success: true, message: "Reset email sent"})
+  } catch (error) {
+    res.status(500);
+    throw new Error("Email not sent! Please try again");
+  }
+
 });
 
 module.exports = {
