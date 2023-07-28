e18213dcdb5b0df3e265c653d305455e76e54193
MarindanyTracy <marindanytracy0@gmail.com>
2023-07-28 14:45:26 +0300

    Get all products - route and controller

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,9 +1,11 @@
 const express = require("express");
-const { createProduct } = require("../controllers/productControllers");
+const { createProduct, getProducts } = require("../controllers/productControllers");
 const router = express.Router();
 const protect = require("../middleware/authMiddleware.js");
 const { upload } = require("../utils/fileUpload");
 
 router.post('/', protect, upload.single("image"), createProduct);
+
+router.get('/', protect, getProducts)
 
 module.exports = router
