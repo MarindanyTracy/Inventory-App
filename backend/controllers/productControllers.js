3a0f59a99653f710ca1795a7d068f154f7c5d1f1
MarindanyTracy <marindanytracy0@gmail.com>
2023-07-28 14:37:12 +0300

    Save product to cloudinary

--- backend/controllers/productControllers.js
+++ backend/controllers/productControllers.js
@@ -1,6 +1,7 @@
 const asyncHandler = require("express-async-handler");
 const Product = require('../models/productModel');
 const { fileSizeFormatter } = require("../utils/fileUpload");
+const cloudinary = require("cloudinary").v2;
 
 const createProduct = asyncHandler( async(req, res) => {
   const {name, sku, category, quantity, price, description } = req.body;
@@ -14,9 +15,19 @@ const createProduct = asyncHandler( async(req, res) => {
 //Handle Image Upload
 let fileData = {}
 if (req.file) {
+  //save file to cloudinary
+  let uploadedFile;
+  try {
+    uploadedFile = await cloudinary.uploader.upload(req.file.path, {
+      folder: "Invent App", resource_type:"image"
+    })
+  } catch (error) {
+    res.status(500);
+    throw new Error("Image could not be uploaded")
+  }
   fileData = {
     fileName: req.file.originalname,
-    filePath: req.file.path,
+    filePath: uploadedFile.secure_url,
     fileType: req.file.mimetype,
     fileSize: fileSizeFormatter(req.file.size, 2),
   }
