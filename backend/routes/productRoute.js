7cacb0cb89fc465c2b4fefcd50c79bc692abb28e
MarindanyTracy <marindanytracy0@gmail.com>
2023-07-28 15:13:57 +0300

    Delete a product - route and controller

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { createProduct, getProducts, getProduct } = require("../controllers/productControllers");
+const { createProduct, getProducts, getProduct, deleteProduct } = require("../controllers/productControllers");
 const router = express.Router();
 const protect = require("../middleware/authMiddleware.js");
 const { upload } = require("../utils/fileUpload");
@@ -9,6 +9,8 @@ router.post('/', protect, upload.single("image"), createProduct);
 router.get('/', protect, getProducts)
 
 router.get('/:id', protect, getProduct)
+
+router.delete('/:id', protect, deleteProduct)
 
 
 module.exports = router
