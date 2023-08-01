7e8b0353e83339447793eec59470d0cf9de57562
MarindanyTracy <marindanytracy0@gmail.com>
2023-08-01 12:47:34 +0300

    Create Auth pages and routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,19 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './Pages/Home/Home';
+import Login from './Pages/Auth/Login';
+import Register from './Pages/Auth/Register';
+import Forgot from './Pages/Auth/Forgot';
+import Reset from './Pages/Auth/Reset';
 
 function App() {
   return (
     <BrowserRouter>
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/forgot" element={<Forgot />} />
+      <Route path="/resetpassword/:resetToken" element={<Reset />} />
     </Routes>
     </BrowserRouter>
   )}
