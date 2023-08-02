6d69acbca1c1572f208ba057f0184ebb13efd451
MarindanyTracy <marindanytracy0@gmail.com>
2023-08-02 10:23:33 +0300

    Footer component

--- frontend/src/components/Footer/Footer.js
+++ frontend/src/components/Footer/Footer.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>
-      
+    <div className='--flex-center --py2'>
+      <p>All Rights Reserved. &copy; 2023</p>
     </div>
   )
 }
