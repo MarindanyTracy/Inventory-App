6c164fdaefb11a024ad8abd85031a701e0f3fc7d
MarindanyTracy <marindanytracy0@gmail.com>
2023-08-03 20:23:29 +0300

    Register User - Part 1

--- frontend/src/Pages/Auth/Register.js
+++ frontend/src/Pages/Auth/Register.js
@@ -1,38 +1,91 @@
-import React from 'react';
-import styles from './auth.module.scss';
-import { TiUserAddOutline } from 'react-icons/ti';
-import Card from '../../components/Card/Card';
-import { Link } from 'react-router-dom';
+import React, { useState } from "react";
+import styles from "./auth.module.scss";
+import { TiUserAddOutline } from "react-icons/ti";
+import Card from "../../components/Card/Card";
+import { Link } from "react-router-dom";
+
+const initialState = {
+  name: "",
+  email: "",
+  password: "",
+  password2: "",
+};
 
 const Register = () => {
+  const [formData, setFormData] = useState(initialState);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const { name, email, password, password2 } = formData;
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({...formData, [name]: value})
+  };
+
+  const register = (e) => {
+    e.preventDefault()
+
+    console.log(formData)
+  };
+
+
   return (
     <div className={`container ${styles.auth}`}>
       <Card>
         <div className={styles.form}>
-          <div className='--flex-center'>
+          <div className="--flex-center">
             <TiUserAddOutline size={35} color="#999" />
           </div>
           <h2>Register</h2>
 
-          <form action="">
-            <input type="text" placeholder='Name' required name="name" />
-            <input type="email" placeholder='Email' required name="email" />
-            <input type="password" placeholder='Password' required name="password" />
-            <input type="password" placeholder='Confirm Password' required name="password" />
+          <form onSubmit={register}>
+            <input
+              type="text"
+              placeholder="Name"
+              required
+              name="name"
+              value={name}
+              onChange={handleInputChange}
+            />
+            <input
+              type="email"
+              placeholder="Email"
+              required
+              name="email"
+              value={email}
+              onChange={handleInputChange}
+            />
+            <input
+              type="password"
+              placeholder="Password"
+              required
+              name="password"
+              value={password}
+              onChange={handleInputChange}
+            />
+            <input
+              type="password"
+              placeholder="Confirm Password"
+              required
+              name="password2"
+              value={password2}
+              onChange={handleInputChange}
+            />
 
-            <button type="submit" className='--btn --btn-primary --btn-block'>Register</button>
-
+            <button type="submit" className="--btn --btn-primary --btn-block">
+              Register
+            </button>
           </form>
-    
+
           <span className={styles.register}>
-          <Link to="/">Home</Link>
-          <p>&nbsp; Already have an account? &nbsp;</p>
-          <Link to="/login">Login</Link>
+            <Link to="/">Home</Link>
+            <p>&nbsp; Already have an account? &nbsp;</p>
+            <Link to="/login">Login</Link>
           </span>
         </div>
       </Card>
     </div>
-  )
-}
+  );
+};
 
-export default Register
+export default Register;
