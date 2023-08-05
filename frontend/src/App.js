62e22adafd4618465f83518ce2ac94376ae15fb8
MarindanyTracy <marindanytracy0@gmail.com>
2023-08-05 21:10:50 +0300

    Format document

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,51 +1,56 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './Pages/Home/Home';
-import Login from './Pages/Auth/Login';
-import Register from './Pages/Auth/Register';
-import Forgot from './Pages/Auth/Forgot';
-import Reset from './Pages/Auth/Reset';
-import Sidebar from './components/Sidebar/Sidebar';
-import Dashboard from './Pages/Dashboard/Dashboard';
-import Layout from './components/Layout/Layout';
-import axios from 'axios';
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import { useDispatch } from 'react-redux';
-import { useEffect } from 'react';
-import { getLoginStatus } from './services/authService';
-import { SET_LOGIN } from './redux/features/auth/authSlice';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home/Home";
+import Login from "./Pages/Auth/Login";
+import Register from "./Pages/Auth/Register";
+import Forgot from "./Pages/Auth/Forgot";
+import Reset from "./Pages/Auth/Reset";
+import Sidebar from "./components/Sidebar/Sidebar";
+import Dashboard from "./Pages/Dashboard/Dashboard";
+import Layout from "./components/Layout/Layout";
+import axios from "axios";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { useDispatch } from "react-redux";
+import { useEffect } from "react";
+import { getLoginStatus } from "./services/authService";
+import { SET_LOGIN } from "./redux/features/auth/authSlice";
 
-axios.defaults.withCredentials = true
+axios.defaults.withCredentials = true;
 
 function App() {
-const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-useEffect(() => {
-  async function loginStatus () {
-    const status = await getLoginStatus()
-    dispatch(SET_LOGIN(status))
-  }
-  loginStatus()
-}, [dispatch])
+  useEffect(() => {
+    async function loginStatus() {
+      const status = await getLoginStatus();
+      dispatch(SET_LOGIN(status));
+    }
+    loginStatus();
+  }, [dispatch]);
+  
   return (
     <BrowserRouter>
-    <ToastContainer />
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<Register />} />
-      <Route path="/forgot" element={<Forgot />} />
-      <Route path="/resetpassword/:resetToken" element={<Reset />} />
+      <ToastContainer />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/forgot" element={<Forgot />} />
+        <Route path="/resetpassword/:resetToken" element={<Reset />} />
 
-      <Route path="/dashboard" element={
-        <Sidebar>
-          <Layout>
-            <Dashboard />
-          </Layout>
-        </Sidebar>
-      } />
-    </Routes>
+        <Route
+          path="/dashboard"
+          element={
+            <Sidebar>
+              <Layout>
+                <Dashboard />
+              </Layout>
+            </Sidebar>
+          }
+        />
+      </Routes>
     </BrowserRouter>
-  )}
+  );
+}
 
 export default App;
