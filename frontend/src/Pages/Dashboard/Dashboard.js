d5c4a22b6d6cb942eefcdc846dae3d30041d47b1
MarindanyTracy <marindanytracy0@gmail.com>
2023-08-08 18:48:54 +0300

    Finishing on get all products

--- frontend/src/Pages/Dashboard/Dashboard.js
+++ frontend/src/Pages/Dashboard/Dashboard.js
@@ -1,11 +1,31 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import useRedirectLoggedOutUser from '../../customHook/useRedirectLoggedOutUser';
+import { useDispatch, useSelector } from 'react-redux';
+import { selectIsLoggedIn } from '../../redux/features/auth/authSlice';
+import ProductList from '../../components/product/productList/ProductList';
+import { getProducts } from "../../redux/features/product/productSlice";
 
 const Dashboard = () => {
   useRedirectLoggedOutUser('/login')
+  const dispatch = useDispatch();
+  const isLoggedIn = useSelector(selectIsLoggedIn);
+
+  const {products, isLoading, isError, message } = useSelector((state) => state.product);
+
+  useEffect(() => {
+    if(isLoggedIn === true) {
+      dispatch(getProducts);
+    
+    }
+
+    if(isError) {
+      console.log(message);
+    }
+  },[isLoggedIn, isError, message, dispatch])
   return (
     <div>
       <h2>Dashboard</h2>
+      <ProductList />
     </div>
   )
 }
