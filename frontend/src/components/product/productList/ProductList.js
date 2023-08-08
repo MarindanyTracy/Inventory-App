1f0e738f69f9f21bbb5b3b94357c506ff40461fb
MarindanyTracy <marindanytracy0@gmail.com>
2023-08-08 19:24:24 +0300

    Display Products on Dashboard

--- frontend/src/components/product/productList/ProductList.js
+++ frontend/src/components/product/productList/ProductList.js
@@ -1,10 +1,82 @@
 import React from 'react';
 import './productList.scss';
+import { FaEdit, FaTrashAlt } from "react-icons/fa";
+import { AiOutlineEye } from "react-icons/ai";
+import { SpinnerImg } from '../../Loader/loader';
 
-const ProductList = () => {
+const ProductList = ({products, isLoading}) => {
+  if(!products) {
+    return <p>Loading...</p>
+  }
+
+  const shortenText = (text, n) => {
+    if(text.length > n) {
+      const shortenedText = text.substring(0,n).concat("...")
+      return shortenedText
+   }
+   return text;
+  }
+
   return (
-    <div>
-      
+    <div className='product-list'>
+      <hr />
+      <div className='table'>
+        <div className='--flex-between --flex-dir-column'>
+          <span>
+            <h3>Inventory Items</h3>
+          </span>
+          <span>
+            <h3>Search products</h3>
+          </span>
+        </div>
+
+        {isLoading && <SpinnerImg /> }
+
+        <div className='table'>
+          {!isLoading && products.length === 0 ? (
+            <p>No products found, please add a product... </p>
+          ): (
+            <table>
+              <thead>
+                <tr>
+                  <th>s/n</th>
+                  <th>Name</th>
+                  <th>Category</th>
+                  <th>Price</th>
+                  <th>Quantity</th>
+                  <th>Value</th>
+                  <th>Actions</th>
+                </tr>
+              </thead>
+
+              <tbody>
+                {
+                  products.map((product,index) => {
+                    const {_id, name, category, price,quantity} = product
+                    return (
+                      <tr key={_id}>
+                        <td>{index + 1}</td>
+                        <td>{shortenText(name, 16)}</td>
+                        <td>{category}</td>
+                        <td>{"$"}{price }</td>
+                        <td>{quantity}</td>
+                        <td>{"$"}{price * quantity}</td>
+                        <td className='icons'>
+                          <span>
+                            <AiOutlineEye size={25} color={"purple"} />
+                            <FaEdit size={20} color={"green"} />
+                            <FaTrashAlt size={20} color={"red"} />
+                          </span>
+                        </td>
+                      </tr>
+                    )
+                  })
+                }
+              </tbody>
+            </table>
+          )}
+        </div>
+      </div>
     </div>
   )
 }
